4309e98225a456be880f2240ad433c5e526f5bd0
Cclaudiavs <124632907+Cclaudiavs@users.noreply.github.com>
2023-12-07 09:51:23 -0300

    se agregaron archivos  de service-worker para transformar en PWA

---
+++ reservas/service-worker.js
@@ -0,0 +1,22 @@
+// service-worker.js
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                // Agrega más archivos estáticos que deseas cachear
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
